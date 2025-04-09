b5492c9e3c358879d25430371aebb368baaa5554
Nandyala Sri Manikanta Subbarao <22h45a6105@bvcits.edu.in>
2025-04-09 08:50:06 +0530

    changed

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,15 @@ import express from "express";
 import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors"; // ✅ ADD THIS
+
+// routes
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+
 // utils
 import connectDB from "./config/db.js";
 
@@ -18,10 +22,18 @@ connectDB();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.PRODUCT_API_URL,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// routes
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
