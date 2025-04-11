662eed9888b338ff7648b6f829f473aad109c71d
Nandyala Sri Manikanta Subbarao <22h45a6105@bvcits.edu.in>
2025-04-11 10:56:33 +0530

    changed the utils

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
   // set JWT as an HTTP-only cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "devlopment",
+    secure: process.env.NODE_ENV !== "development",
     sameSite: "strict",
     maxAge: 30 * 24 * 60 * 100,
   });
