3eb2ec567dbeb9db81f690ccba3fcd84a63e8909
Nandyala Sri Manikanta Subbarao <22h45a6105@bvcits.edu.in>
2025-04-22 21:46:08 +0530

    changed

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -15,6 +15,7 @@ import { BASE_URL } from "../constants";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
+  credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.userInfo?.token;
     if (token) {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://zithara-rp92.onrender.com",
+      "/api": "http://localhost:5000",
     },
   },
 });
