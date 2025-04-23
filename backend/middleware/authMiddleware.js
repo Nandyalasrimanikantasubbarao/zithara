caba67ac19a3ec4166ebb19eae473bd80800c0fa
Nandyala Sri Manikanta Subbarao <22h45a6105@bvcits.edu.in>
2025-04-23 10:58:51 +0530

    change authentication

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,12 +2,34 @@ import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 import asyncHandler from "./asyncHandler.js";
 
+// const authenticate = asyncHandler(async (req, res, next) => {
+//   let token;
+//   token = req.cookies.jwt;
+
+//   if (token) {
+//     try {
+//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//       req.user = await User.findById(decoded.userId).select("-password");
+//       next();
+//     } catch (err) {
+//       res.status(401);
+//       throw new Error("Not authorized, token failed");
+//     }
+//   } else {
+//     res.status(401);
+//     throw new Error("Not authorized no token");
+//   }
+// });
+
 const authenticate = asyncHandler(async (req, res, next) => {
   let token;
-  token = req.cookies.jwt;
 
-  if (token) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
     try {
+      token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.userId).select("-password");
       next();
@@ -17,7 +39,7 @@ const authenticate = asyncHandler(async (req, res, next) => {
     }
   } else {
     res.status(401);
-    throw new Error("Not authorized no token");
+    throw new Error("Not authorized, no token");
   }
 });
 
